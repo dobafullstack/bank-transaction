9396f446378907b8b0a4d3b1772010d4b7697437
Doba <doba.fullstack@gmail.com>
2022-07-16 15:26:41 +0700

    fix_cors_3

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://bank-transaction-qyk83vopr-dobafullstack.vercel.app/:path*',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
